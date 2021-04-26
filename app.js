b2d992c3ba55e7a1d915fad07c8845a888ad334e
AllisonWonderland <aneary91@gmail.com>
2021-04-26 11:29:20 -0500

    added to my app.js

--- app.js
+++ app.js
@@ -46,10 +46,115 @@ const startApp = () => {
             var manager = Manager(name, id, email, officeNumber);
 
             teamMember.push(manager);
-            
+
             })     
         ]);
         createManager();
 }
 startApp();
+createTeam();
 }
+
+
+function createTeam() {
+
+    inquirer
+        .prompt([
+        {
+            type: "list",
+            name: "command",
+            message:"Would you like to add more team members?",
+            choices:["Add an Engineer", "Add an Intern", "Make team"]
+        }
+        ])
+        .then(answers => {
+            statement = answers.command;
+
+            switch(statement){
+                case "Add an Engineer":
+                    getEngineer();
+                    break;
+
+                case "Add an Intern":
+                    getIntern();
+                    break;
+
+                case "Make team":
+                    buildTeam();
+                    break;
+            }
+        })
+}
+
+function getEngineer() {
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the engineers name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is the engineers id?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is the engineers email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is the engineers github username?",
+                name: "github"
+            }
+
+        ])
+        .then(answers => {
+            var {name, id, email, github} = answers;
+            var engineer = Engineer(name, id, email, github);
+            teamMember.push(engineer);
+        })
+
+}
+function getIntern() {
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the Interns name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is the Interns id?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is the Interns email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is the school name?",
+                name: "school"
+            }
+
+        ])
+        .then(answers => {
+            var {name, id, email, school} = answers;
+            var intern = Intern(name, id, email, school);
+
+        })
+
+}
+
+function buildTeam() {
+    fs.writeFileSync(outputPath, mainRender(teamMember), "utf-8");
+}
+
+startApp()
