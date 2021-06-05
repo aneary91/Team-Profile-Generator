01ecd51d9601bcf61db14aeb96995ddf98fed303
AllisonWonderland <aneary@gmail.com>
2021-06-05 08:41:45 -0500

    added the test for the office number

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,18 +1,22 @@
-const Employee = require("./Employee");
+const Intern = require("../lib/Intern");
 
-class Intern extends Employee{
+// testing school parameters
+test("testing to see if the school parameter is working", () => {
+  const school = "SMU";
+  const int = new Intern("Allison", 1234, "aneary91@gmail.com", school);
+  expect(int.school).toBe(school);
+});
 
-    constructor(name, id, email, github){
-       
-        super(name, id, email)
-        this.github = github;
+// testing getSchool and getRole methods
 
-    }
-    getSchool(){
+test("testing to see if the getRole method gets the role", () => {
+  const role = "Intern";
+  const int = new Intern("Allison", 1234, "aneary91@gmail.com", role);
+  expect(int.getRole()).toBe(role);
+});
 
-    }
-    
-    getRole(){
-        return 'Intern'
-    }
-}
+test('testing to see if the getSchool returns the school', () =>{
+    const college = 'SMU';
+    const int = new Intern("Allison", 1234, "aneary91@gmail.com", college);
+    expect(int.getSchool()).toBe(college);
+});
