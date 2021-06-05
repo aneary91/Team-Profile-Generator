f2bec2b21d2d981b375e6e5266806b22655715ad
AllisonWonderland <aneary@gmail.com>
2021-06-05 09:15:55 -0500

    got the tests all to pass

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -2,16 +2,16 @@ const Manager = require("../lib/Manager");
 
 // testing office number parameters
 test('testing to see if the office number parameter is working.', () => {
-    const theOfficeNumber = '911';
+    const officeNumber = '911';
     const mngr = new Manager("Allison", 1234, "aneary91@gmail.com", officeNumber);
-    expect(mngr.theOfficeNumber).toBe(theOfficeNumber);  
+    expect(mngr.officeNumber).toBe(officeNumber);  
 });
 
 // testing office number and get role methods
 test('testing to see if the office number method returns office number', () => {
     const phoneNumber = '911';
-    const mngr = new Manager("Allison", 1234, "aneary91@gmail.com", officeNumber);
-    expect(mngr.phoneNuber()).toBe(phoneNumber);
+    const mngr = new Manager("Allison", 1234, "aneary91@gmail.com", phoneNumber);
+    expect(mngr.getOfficeNumber()).toBe(phoneNumber);
 }); 
 
 test('testing to see if the getRole method returns the role', () => {
